c061389e0fff600ed341b2af8883ff5043d0fc75
lincoln ferreira <lincoln.carmo@sptech.school>
2025-02-24 23:45:19 -0300

    configurando compose

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import {
   ExpressAdapter,
@@ -10,15 +10,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-// Create an Express server
 const server: express.Express = express();
 
-// Define a function to create a NestJS server within an Express instance
 export const createNestServer = async (expressInstance: express.Express) => {
-  // Create an ExpressAdapter with the Express instance
   const adapter = new ExpressAdapter(expressInstance);
 
-  // Create a NestJS application using the AppModule and the adapter
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
     adapter,
@@ -36,17 +32,14 @@ export const createNestServer = async (expressInstance: express.Express) => {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  // Enable CORS for the application
   app.enableCors();
-  // Initialize the application and return it
+
   await app.listen(3000);
   return app.init();
 };
 
-// Create the NestJS server and log the status
 createNestServer(server)
-  .then((v) => console.log('Nest Ready')) // Log success message
-  .catch((err) => console.error('Nest broken', err)); // Log error message
+  .then((v) => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
 
-// Export the Express server as a Firebase cloud function
 export const api: functions.HttpsFunction = functions.https.onRequest(server);
